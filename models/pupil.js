91d27346f24b0185e17698906f4787d4d5a87034
Neil Campbell <neildcampbell@hotmail.com>
2019-11-18 09:30:41 +0000

    security changes

--- models/pupil.js
+++
@@ -1,9 +0,0 @@
-var pupilSchema= mongoose.Schema({
-    name:String,
-    email:String,
-    year:Number,
-    points:Number
-    
-})
-
-var Pupil = mongoose.model("Pupil", pupilSchema);
--- models/score.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-
-var scoreSchema = mongoose.Schema({
-    score:Number
-});
-
-module.exports = mongoose.model("Score", scoreSchema)
