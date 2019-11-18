b4244a5c2494dcdd424cb4e09f139d6a45005a43
Neil Campbell <neildcampbell@hotmail.com>
2019-11-18 13:44:48 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
-<?php
-header("Location: https://www.snowytopmaths.co.uk");
-die();
 
-?>
-<script type="text/javascript">
-window.location.href = "https://www.snowytopmaths.co.uk/";
-
-</script>
 
