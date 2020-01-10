df4162d789cd107627874b427caf647a887153dc
Neil Campbell <neildcampbell@hotmail.com>
2020-01-10 16:30:18 +0000

    refactored css

---
+++ public/js/showTicks10.js
@@ -0,0 +1,6 @@
+showTicks10 = () => {
+    document.getElementById("tick").style.visibility = "visible";
+    document.getElementById("msg").style.visibility = "visible";
+    document.getElementById("msg").textContent = " Well done! 10 points";
+    correct ++;
+  }
