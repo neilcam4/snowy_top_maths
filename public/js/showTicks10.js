1f1e1032a2703f4ee9a456ff410a731ce1b10d2a
Neil Campbell <neildcampbell@hotmail.com>
2020-01-11 20:35:43 +0000

    refactored showTicks

--- public/js/showTicks10.js
+++ public/js/showTicks10.js
@@ -4,3 +4,17 @@ showTicks10 = () => {
     document.getElementById("msg").textContent = " Well done! 10 points";
     correct ++;
   }
+
+  showTicks20 = () => {
+    document.getElementById("tick").style.visibility = "visible";
+    document.getElementById("msg").style.visibility = "visible";
+    document.getElementById("msg").textContent = " Well done! 20 points";
+    correct ++;
+  }
+
+  showTicks30 = () => {
+    document.getElementById("tick").style.visibility = "visible";
+    document.getElementById("msg").style.visibility = "visible";
+    document.getElementById("msg").textContent = " Well done! 30 points";
+    correct ++;
+  }
