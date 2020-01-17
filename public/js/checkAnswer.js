81353ddd31d800134560779fa0d364343b798ca1
Neil Campbell <neildcampbell@hotmail.com>
2020-01-17 15:02:48 +0000

    addProgress added and functional for chapter 1

--- public/js/checkAnswer.js
+++ public/js/checkAnswer.js
@@ -94,31 +94,5 @@ function getScore() {
         }
     });
 }
-function addProgress() {
-    console.log("add progress function")
-    var percentage = correct/6 * 100
-    var title = $("#heading").text()
-    var id = $("#userId").text();
-    var url = "https://api.mlab.com/api/1/databases/maths_app/collections/users/";
-    var api = $("#example").text();
-    var sliced = id.slice(0, -1);
-    var finalApi = url + sliced + api;
-    $.ajax({
-        url: finalApi,
-        type: "put",
-        data: JSON.stringify({
-            "$push": {
-                "quiz": title + ":" + percentage + "%"
-            }
-        }),
-        contentType: "application/json",
-        success: function (data) {
-            getScore();
-            $("#jquery").text(data.score);
-        },
-        error: function (xhr, status, error) {
-            console.log(error);
-        }
-    });
-};
+
 
