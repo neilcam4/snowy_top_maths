ac10271997a9817938d16516ad8a4289a9d8edb0
Neil Campbell <neildcampbell@hotmail.com>
2020-01-24 17:19:09 +0000

    added data3 and all users loop

--- models/quiz.js
+++ models/quiz.js
@@ -1,13 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 var quizSchema = Schema({
-    name:String,
-    description:String,
-    // categories:[subCategory],
-    // questions:[subQuestions],
-    // score:[totalScore]
+    quiz:String
 });
-
-module.exports = mongoose.model('Quiz', quizSchema);
+var Quiz = mongoose.model("Quiz", quizSchema)
+module.exports = Quiz;
--- public/js/trackProgress6.js
+++ public/js/trackProgress6.js
@@ -1,3 +1,17 @@
+// require('../../models/user')
+
+// function trackProgress6(){
+//     var title = $("#heading").text()
+//     var percentage = Math.ceil(correct/6 * 100)
+//     User.findOne({username:"Neil Admin"}, function(error, user){
+//     if(error){
+//         console.log("THere has been an error" + error)
+//     } else {
+//         console.log("track progress6 working" + user)
+//     }
+// })
+// }
+
 function trackProgress6() {
     console.log("add progress function")
     var percentage = Math.ceil(correct/6 * 100)
@@ -12,8 +26,8 @@ function trackProgress6() {
         type: "put",
         data: JSON.stringify({
             "$push": {
-                "quiz": title + ":" + percentage + "%"
-            }
+                    "quiz": title
+            }   
         }),
         contentType: "application/json",
         success: function (data) {
